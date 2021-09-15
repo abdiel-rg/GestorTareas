513558579901ff3619f689de7226c116ca6cd4e8
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-09-15 16:20:48 -0400

    Add LocalStorageRepository

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using GestorTareas.Data;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,8 @@ namespace GestorTareas
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<LocalStorageRepository>();
 
             await builder.Build().RunAsync();
         }
