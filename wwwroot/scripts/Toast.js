e19205ef93bd619b494486114d9e3b2622c97b13
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-09-22 17:45:26 -0400

    Create Toast component

---
+++ wwwroot/scripts/Toast.js
@@ -0,0 +1,11 @@
+﻿export const showToast = (toastId) => {
+    let reference = document.querySelector(`#${toastId}`);
+    let toast = new bootstrap.Toast(reference);
+    toast.show();
+}
+
+export const hideToast = (toastId) => {
+    let reference = document.querySelector(`#${toastId}`);
+    let toast = new bootstrap.Toast(reference);
+    toast.hide();
+}
