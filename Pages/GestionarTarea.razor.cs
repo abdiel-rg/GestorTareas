e2cc2fb6c770e19b1b04ca8d391f53707a47d0e4
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-09-23 13:58:27 -0400

    Modify GestionarTarea styles
    Delete ParsedID property

--- Pages/GestionarTarea.razor.cs
+++ Pages/GestionarTarea.razor.cs
@@ -13,14 +13,13 @@ namespace GestorTareas.Pages
         private Tarea tarea = new();
 
         private string ButtonText => ID is null ? "Agregar Tarea" : "Editar Tarea";
+        private string ButtonClass => ID is null ? "btn btn-primary" : "btn btn-success";
+        private string ButtonIconClass => ID is null ? "oi oi-plus" : "oi oi-pencil";
 
-        private Task<Tarea?> TareaABuscar => tareaRepository.Get(t => t.ID == ParsedID);
-
+        private Task<Tarea?> TareaABuscar => tareaRepository.Get(t => t.ID.ToString() == ID);
 
         [Parameter]
         public string? ID { get; set; }
-
-        private Guid ParsedID => ID is not null ? Guid.Parse(ID) : tarea.ID;
 
         private async Task SaveTarea(Tarea tarea)
         {
