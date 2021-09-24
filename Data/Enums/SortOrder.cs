9893ef90555f62e65f454c9f8165bdbf8de1c6f6
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-09-24 16:54:59 -0400

    Move SortOrder file

--- Data/Enums/SortOrder.cs
+++ Data/Enums/SortOrder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GestorTareas.Data
+namespace GestorTareas.Data.Enums
 {
     public enum SortOrder
     {
