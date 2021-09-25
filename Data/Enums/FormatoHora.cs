09c1df28975de6a7a0bbd3a614b2881549e2c94e
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-09-25 14:44:16 -0400

    Create FormatoHora enum

---
+++ Data/Enums/FormatoHora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorTareas.Data.Enums
+{
+    public enum FormatoHora
+    {
+        hh = 12,
+        HH = 24
+    }
+}
