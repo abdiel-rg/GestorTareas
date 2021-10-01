b5a91d7f3c25185357dc18386dc10b0682d849d0
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-10-01 11:42:38 -0400

    Create JsonValidator.cs

---
+++ Data/JsonValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestorTareas.Data
+{
+    public class JsonValidator : ValidationAttribute
+    {
+
+        public JsonValidator(Type typeToDeserialize) => TypeToDeserialize = typeToDeserialize;
+
+        private Type TypeToDeserialize { get; set; } = typeof(object);
+
+        public override bool IsValid(object? value)
+        {
+            try
+            {
+                if (value is null) return false;
+                object? json = JsonSerializer.Deserialize(value.ToString()!, TypeToDeserialize);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) => IsValid(value) ? ValidationResult.Success : new(ErrorMessage);
+    }
+}
